101e8bd69debae7fb96b8ce9fafa2e92bc63d557
Angelo Marques <angelomarquesdeoliveira@gmail.com>
2025-07-31 04:24:42 -0300

    Quase final.

---
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/CartsController.cs
@@ -0,0 +1,165 @@
+﻿using Ambev.DeveloperEvaluation.Application.Carts.CreateCart.Commands;
+using Ambev.DeveloperEvaluation.Application.Carts.DeleteCart.Commands;
+using Ambev.DeveloperEvaluation.Application.Carts.DeleteCart.Responses;
+using Ambev.DeveloperEvaluation.Application.Carts.GetCart.Commands;
+using Ambev.DeveloperEvaluation.Application.Carts.GetCart.Responses;
+using Ambev.DeveloperEvaluation.Application.Carts.ListCarts.Responses;
+using Ambev.DeveloperEvaluation.Application.Carts.UpdateCart.Commands;
+using Ambev.DeveloperEvaluation.Application.Carts.UpdateCart.Responses;
+using Ambev.DeveloperEvaluation.Application.Pagination;
+using Ambev.DeveloperEvaluation.WebApi.Common;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts
+{
+
+    [Route("api/carts")]
+    public class CartsController : BaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public CartsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Creates a new cart
+        /// </summary>
+        /// <param name="request">The cart creation request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created cart details</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateCartAsync([FromBody] CreateCartCommand request, CancellationToken cancellationToken)
+        {
+
+            var command = _mapper.Map<CreateCartCommand>(request);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return Created(nameof(GetCartByIdAsync), response);
+        }
+
+        /// <summary>
+        /// Retrieves a cart by their ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cart.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The cart details if found, otherwise an appropriate error response.</returns>
+        [HttpGet("{id}", Name = "GetCartByIdAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCartByIdAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest(new { Message = "Invalid cart ID." });
+            }
+
+            var command = _mapper.Map<GetCartCommand>(id);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            if (response == null)
+            {
+                return NotFound(new { Message = "Cart not found." });
+            }
+
+            return Ok(new ApiResponseWithData<GetCartResponse>
+            {
+                Success = true,
+                Message = "Cart retrieved successfully",
+                Data = _mapper.Map<GetCartResponse>(response)
+            });
+            
+        }
+
+        /// <summary>
+        /// Retrieves a paginated list of carts.
+        /// </summary>
+        /// <param name="pageNumber">Page number (must be 1 or greater).</param>
+        /// <param name="pageSize">Number of carts per page (must be between 1 and 100).</param>
+        /// <param name="order">Sorting order (optional).</param>
+        /// <param name="filter">Filters to apply (optional).</param>
+        /// <returns>Paginated list of carts.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCartPageAsync([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10, [FromQuery] string? order = null)
+        {
+            var query = new PaginationQuery<ListCartResponse>(pageNumber, pageSize, order);
+
+            Application.Pagination.PaginatedResponse<ListCartResponse> result = await _mediator.Send(query);
+
+            return Ok(new ApiResponseWithData<ListCartResponse>
+            {
+                Success = true,
+                Message = "Cart retrieved successfully",
+                Data = _mapper.Map<ListCartResponse>(result)
+            });
+        }
+
+        /// <summary>
+        /// Updates an existing cart
+        /// </summary>
+        /// <param name="id">The unique identifier of the cart to update</param>
+        /// <param name="request">The cart update request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The updated cart details</returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateCartAsync([FromRoute] Guid id, [FromBody] UpdateCartCommand request, CancellationToken cancellationToken)
+        {
+            // Garante que o ID da rota seja utilizado no comando
+            request.Id = id;
+
+            // Envia o comando direto ao Mediator, confiando que os middlewares e validators já garantem a integridade dos dados
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return Ok(new ApiResponseWithData<UpdateCartResponse>
+            {
+                Success = true,
+                Message = "Cart update successfully",
+                Data = _mapper.Map<UpdateCartResponse>(response)
+            });
+        }
+
+        /// <summary>
+        /// Deletes a cart by their ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cart to delete.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>No content if the cart was deleted, or an error response.</returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteCartAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var command = _mapper.Map<DeleteCartCommand>(id);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            if (response == null)
+            {
+                return NotFound(new { Message = "Cart not found." });
+            }
+            return Ok(new ApiResponseWithData<DeleteCartResponse>
+            {
+                Success = true,
+                Message = "Cart delete successfully",
+                Data = _mapper.Map<DeleteCartResponse>(response)
+            });
+        }
+    }
+
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Products/ProductsController.cs
@@ -0,0 +1,171 @@
+﻿using Ambev.DeveloperEvaluation.Application.Carts.GetCart.Responses;
+using Ambev.DeveloperEvaluation.Application.Pagination;
+using Ambev.DeveloperEvaluation.Application.Products.CreateProduct.Commands;
+using Ambev.DeveloperEvaluation.Application.Products.CreateProduct.Responses;
+using Ambev.DeveloperEvaluation.Application.Products.DeleteProduct.Commands;
+using Ambev.DeveloperEvaluation.Application.Products.DeleteProduct.Responses;
+using Ambev.DeveloperEvaluation.Application.Products.GetProduct.Commands;
+using Ambev.DeveloperEvaluation.Application.Products.GetProduct.Responses;
+using Ambev.DeveloperEvaluation.Application.Products.GetProductCategories;
+using Ambev.DeveloperEvaluation.Application.Products.GetProductsByCategory;
+using Ambev.DeveloperEvaluation.Application.Products.ListProducts;
+using Ambev.DeveloperEvaluation.Application.Products.ListProducts.Responses;
+using Ambev.DeveloperEvaluation.Application.Products.UpdateProduct.Commands;
+using Ambev.DeveloperEvaluation.Application.Products.UpdateProduct.Responses;
+using Ambev.DeveloperEvaluation.WebApi.Common;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OneOf.Types;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products
+{
+    [Route("api/products")]
+    public class ProductsController : BaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateProductAsync([FromBody] CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var command = _mapper.Map<CreateProductCommand>(request);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return Created(nameof(GetProductByIdAsync), _mapper.Map<CreateProductResponse>(response));
+        }
+
+        [HttpGet("{id}", Name = nameof(GetProductByIdAsync))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProductByIdAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest(new { Message = "Invalid product ID." });
+            }
+
+            var command = _mapper.Map<GetProductCommand>(id);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            if (response == null)
+            {
+                return NotFound(new { Message = "Product not found." });
+            }
+
+            return Ok(new ApiResponseWithData<GetProductResponse>
+            {
+                Success = true,
+                Message = "Product successfully",
+                Data = _mapper.Map<GetProductResponse>(response)
+            });
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProductsPageAsync([FromQuery] int pageNumber = 1,
+                                                              [FromQuery] int pageSize = 10,
+                                                              [FromQuery] string? order = null,
+                                                              [FromQuery] ListProductsQuery? filter = null)
+        {
+            var query = new PaginationQuery<ListProductsQuery, ListProductResponse>(pageNumber, pageSize, order, filter);
+
+            Application.Pagination.PaginatedResponse<ListProductResponse> result = await _mediator.Send(query);
+
+            if (result == null)
+            {
+                return NotFound(new { Message = "Product not found." });
+            }
+
+            return Ok(new ApiResponseWithData<ListProductResponse>
+            {
+                Success = true,
+                Message = "Product list successfully",
+                Data = _mapper.Map<ListProductResponse>(result)
+            });
+        }
+   
+        [HttpGet("categories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _mediator.Send(new GetProductCategoriesQuery());
+            return Ok(categories);
+        }
+   
+        [HttpGet("category/{category}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProductsByCategory(string category,
+                                                               [FromQuery] int page = 1,
+                                                               [FromQuery] int size = 10,
+                                                               [FromQuery] string? order = null)
+        {
+            var query = new GetProductsByCategoryQuery(category, page, size, order);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateProductAsync([FromRoute] Guid id, [FromBody] UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            request.Id = id;
+
+            var response = await _mediator.Send(request, cancellationToken);
+
+            if (response == null)
+            {
+                return NotFound(new { Message = "Product not found." });
+            }
+
+            return Ok(new ApiResponseWithData<UpdateProductResponse>
+            {
+                Success = true,
+                Message = "Product upadate successfully",
+                Data = _mapper.Map<UpdateProductResponse>(response)
+            });
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteProductAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var command = _mapper.Map<DeleteProductCommand>(id);
+            var result = await _mediator.Send(command, cancellationToken);
+
+            if (result == null)
+            {
+                return NotFound(new { Message = "Product not found." });
+            }
+
+            return Ok(new ApiResponseWithData<DeleteProductResponse>
+            {
+                Success = true,
+                Message = "Product delete successfully",
+                Data = _mapper.Map<DeleteProductResponse>(result)
+            });
+        
+        }
+    }
+}
